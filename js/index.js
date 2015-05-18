660c12f0e9910af724c2f5c8c5a1d14512275d64
nsn972 <nsn972@gmail.com>
2015-05-18 17:52:04 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var cardTurnOn = function(crd, col) {
   var card = document.getElementById(crd);
   card.style.backgroundColor = (col);
+ 
 };
 
 var cardTurnOff = function(crd, col) {
@@ -14,25 +15,25 @@ var cardTurnOnRandom = function() {
 
   if (x < 25) {
     cardTurnOn('card1', 'red');
-    timeCard('card1', 1000, '#FFCCCC');
+    timeCard('card1', 500, '#FFCCCC');
 
   }
 
   if (x < 50 && x > 25) {
     cardTurnOn('card2', 'green');
-    timeCard('card2', 1000, '#CCE6CC');
+    timeCard('card2', 500, '#CCE6CC');
 
   }
 
   if (x < 75 && x > 50) {
     cardTurnOn('card3', 'blue');
-    timeCard('card3', 1000, '#CCCCFF');
+    timeCard('card3', 500, '#CCCCFF');
 
   }
 
   if (x < 100 && x > 75) {
     cardTurnOn('card4', 'yellow');
-    timeCard('card4', 1000, '#FFFFCC');
+    timeCard('card4', 500, '#FFFFCC');
 
   }
 
@@ -54,10 +55,10 @@ var startGame = function() {
     setTimeout(function() { //  call a 3s setTimeout when the loop is called
       cardTurnOnRandom(); //  your code here
       i++; //  increment the counter
-      if (i < 10) { //  if the counter < 10, call the loop function
+      if (i < 20) { //  if the counter < 10, call the loop function
         myLoop(); //  ..  again which will trigger another 
       } //  ..  setTimeout()
-    }, 2000)
+    }, 500)
   }
 
   myLoop();
